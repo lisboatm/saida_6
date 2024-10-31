f5e911948c0a82afe1f8540a6cf3c0690e234351
Thiago Lisboa <36263673+lisboatm@users.noreply.github.com>
2024-10-31 19:55:26 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Armazenando a frase em uma variável
+frase = "AMO FAZER EXERCICIO NO URI"
+
+# Exibindo a frase em diferentes formatos
+print(f"<{frase}>")
+print(f"<{frase:>30}>")
+print(f"<{frase:.20}>")
+print(f"<{frase:<20}>")
+print(f"<{frase:<30}>")
+print(f"<{frase:.30}>")
+print(f"<{frase:>30.20}>")
+print(f"<{frase:<30.20}>")
